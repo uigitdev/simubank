b0e566dfa427097855fe460662956f687713e9f0
uigitdev <ui.gitdev@gmail.com>
2025-03-22 22:40:40 +0100

    Add app icons class

---
+++ lib/core/res/icon/app_icons.dart
@@ -0,0 +1,8 @@
+class AppIcon{
+  AppIcon._();
+
+  static String prefix = 'assets/icon/';
+
+  static String get  password => '${prefix}password.svg';
+  static String get  showPassword => '${prefix}show_password.svg';
+}
