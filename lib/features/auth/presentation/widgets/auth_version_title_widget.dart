5370ff498aa2fd48ad88c7dc26662de9f5de50a8
uigitdev <ui.gitdev@gmail.com>
2025-03-22 20:45:26 +0100

    Update version title widget name

--- lib/features/auth/presentation/widgets/auth_version_title_widget.dart
+++ lib/features/auth/presentation/widgets/auth_version_title_widget.dart
@@ -1,7 +1,7 @@
 import 'package:simubank/simubank.dart';
 
-class VersionTitleWidget extends StatelessWidget {
-  const VersionTitleWidget({super.key});
+class AuthVersionTitleWidget extends StatelessWidget {
+  const AuthVersionTitleWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
