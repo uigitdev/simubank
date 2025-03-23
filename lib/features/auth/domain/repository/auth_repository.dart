8174ccc0619471bb627ca950ba19120aafb45071
uigitdev <ui.gitdev@gmail.com>
2025-03-23 19:04:13 +0100

    Update auth repository with correct entity

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -2,5 +2,5 @@ import 'package:simubank/simubank.dart';
 
 abstract class AuthRepository {
   Future<ObfuscationKeyEntity?> getObfuscationKey();
-  Future<SessionModel?> postAuthLogin({required String email, required String password});
+  Future<SessionEntity?> postAuthLogin({required String email, required String password});
 }
