4f5409c34a438a885e7f800614b92bf7b1fdde9c
uigitdev <ui.gitdev@gmail.com>
2025-03-29 20:34:29 +0100

    Set makeCollectionsUnmodifiable value

--- lib/features/home/domain/entities/search/search_transaction_entity.dart
+++ lib/features/home/domain/entities/search/search_transaction_entity.dart
@@ -4,7 +4,7 @@ import 'package:simubank/simubank.dart';
 part 'search_transaction_entity.freezed.dart';
 
 @immutable
-@Freezed(makeCollectionsUnmodifiable: false)
+@Freezed(makeCollectionsUnmodifiable: true)
 class SearchTransactionEntity with _$SearchTransactionEntity {
   @override
   final String searched;
